d52443fc72d117113f116c7e3ef9e73554ba5e67
GraySS <doublebump123@gmail.com>
2025-04-22 02:37:34 +0200

    feat: add builtins header file with command structure and function declarations

---
+++ include/builtins.h
@@ -0,0 +1,20 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+#include "execution.h"
+
+typedef struct {
+    const char *name;
+    int (*func)(command_t *cmd);
+
+} builtin_cmd_t;
+
+int is_builtin(const char *cmd);
+int execute_builtin_command(command_t *cmd);
+
+int builtin_exit(command_t *cmd);
+int builtin_pwd(command_t *cmd);
+int builtin_cd(command_t *cmd);
+int builtin_pwd(command_t *cmd);
+
+#endif //BUILTINS_H
